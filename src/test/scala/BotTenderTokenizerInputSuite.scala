a1074a612d020f2b8e7344628997047ab29514b1
Ivan R. Vecerina <IvanR.vecerina@gmail.com>
2023-03-14 17:39:55 +0100

    Update BotTenderTokenizerInputSuite.scala

--- src/test/scala/BotTenderTokenizerInputSuite.scala
+++ src/test/scala/BotTenderTokenizerInputSuite.scala
@@ -31,8 +31,28 @@ class BotTenderTokenizerInputSuite extends AnyPropSpec with TableDrivenPropertyC
         evaluateInput("santé !") should equal(true)
     }
 
-    // TESTS FOR spellCheckerService
-    property("checking spellchecker") {
-        spellCheckerSvc.getClosestWordInDictionary("_efef") should equal("_efef")
+    // TESTS FOR stringDistance
+    property("two identical strings") {
+        spellCheckerSvc.stringDistance("test", "test") should equal(0)
+    }
+
+    property("two empty strings") {
+        spellCheckerSvc.stringDistance("", "") should equal(0)
+    }
+
+    property("two completely different strings") {
+        spellCheckerSvc.stringDistance("hello", "goodbye") should equal(7)
+    }
+
+    property("two strings with one letter difference") {
+        spellCheckerSvc.stringDistance("hello", "hella") should equal(1)
+    }
+
+    property("two strings that are the same except one has extra characters") {
+        spellCheckerSvc.stringDistance("apple", "applesauce") should equal(5)
+    }
+
+    property("two strings that are almost the same but with some missing characters:") {
+        spellCheckerSvc.stringDistance("beach", "bach") should equal(1)
     }
 }
