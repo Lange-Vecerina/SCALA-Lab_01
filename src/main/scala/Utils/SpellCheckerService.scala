caa43f854902f0e3a5b71389cbe467af8da0dbd4
IvanR-Vecerina <IvanR.vecerina@gmail.com>
2023-03-18 23:43:36 +0100

    Update SpellCheckerService.scala

--- src/main/scala/Utils/SpellCheckerService.scala
+++ src/main/scala/Utils/SpellCheckerService.scala
@@ -25,33 +25,41 @@ trait SpellCheckerService:
 end SpellCheckerService
 
 class SpellCheckerImpl(val dictionary: Map[String, String]) extends SpellCheckerService:
-  // TODO - Part 1 Step 2
+  // Levenstein distance algorithm : This algorithm is used to calculate the distance between two words.
   def stringDistance(s1: String, s2: String): Int = 
+    // Map to store the results of the recursive function "stringDistanceRec" to avoid recalculating the same substrings
     val memo = collection.mutable.Map[(Int, Int), Int]()
+
+    // Recursive function to calculate the Levenstein distance between two words
     def stringDistanceRec(i: Int, j: Int): Int =
+      // If the substring has already been calculated, we just return the result
       if memo.contains((i, j)) then memo((i, j))
       else
+        // Otherwise, we calculate the distance between the two words
         val res = (i, j) match
-          case (0, _) => j
-          case (_, 0) => i
-          case _ =>
-            val cost = if s1(i - 1) == s2(j - 1) then 0 else 1
-            val deletion = stringDistanceRec(i - 1, j) + 1
-            val insertion = stringDistanceRec(i, j - 1) + 1
-            val substitution = stringDistanceRec(i - 1, j - 1) + cost
-            deletion min insertion min substitution
-        memo((i, j)) = res
-        res
+          case (0, _) => j // If one of the words is empty, the distance is the length of the other word
+          case (_, 0) => i // If one of the words is empty, the distance is the length of the other word
+          case _ =>  // Otherwise, we calculate the distance between the two words
+            val cost = if s1(i - 1) == s2(j - 1) then 0 else 1 // If the last characters are the same, the cost is 0, otherwise it is 1
+            val deletion = stringDistanceRec(i - 1, j) + 1 // We calculate the distance between the two words by removing the last character of the first word
+            val insertion = stringDistanceRec(i, j - 1) + 1 // We calculate the distance between the two words by removing the last character of the second word
+            val substitution = stringDistanceRec(i - 1, j - 1) + cost // We calculate the distance between the two words by removing the last character of both words
+            deletion min insertion min substitution // We return the minimum distance between the two words between the three possibilities
+        memo((i, j)) = res // We store the result in the map to avoid recalculating the same substrings
+        res // We return the result
     end stringDistanceRec
-    stringDistanceRec(s1.length, s2.length)
+    stringDistanceRec(s1.length, s2.length) // We call the recursive function
   end stringDistance
 
-  // TODO - Part 1 Step 2
+  // Get the closest word in the dictionary from the given misspelled word
   def getClosestWordInDictionary(misspelledWord: String): String = 
+    // If the word is a number or a pseudonym, we just return it
     if misspelledWord.forall(_.isDigit) then misspelledWord
     else if misspelledWord.startsWith("_") then misspelledWord
     else
+      // Otherwise, we get the closest word in the dictionary from the given misspelled word
       val closestWord = dictionary.keys.minBy(word => stringDistance(word, misspelledWord))
+      // If the distance between the closest word and the misspelled word is greater than 2, we return the misspelled word
       dictionary(closestWord)
   end getClosestWordInDictionary
 end SpellCheckerImpl
