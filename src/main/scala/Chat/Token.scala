209e91f74b0f90297c6af8f11d8f5395c6443413
Ivan R. Vecerina <IvanR.vecerina@gmail.com>
2023-03-28 16:57:59 +0200

    Update Token.scala

--- src/main/scala/Chat/Token.scala
+++ src/main/scala/Chat/Token.scala
@@ -4,18 +4,31 @@ enum Token:
   case // Terms
        // TODO - Part 2 Step 1
        BONJOUR, 
+       QUEL,
+       LE,
+       PRIX,
+       DE,
+       COMBIEN,
        JE,
+       ME,
+       MON,
+       SOLDE,
        SVP,
        ASSOIFFE,
        AFFAME,
        // Actions
        ETRE,
        VOULOIR,
+       COMMANDER,
+       CONNAITRE,
+       COUTER,
+       APPELLER,
        // Logic Operators
        ET,
        OU,
        // Products
        PRODUIT,
+       MARQUE,
        // Util
        PSEUDO,
        NUM,
