10349359f347886d2b7c66796af494264930cea4
IvanR-Vecerina <IvanR.vecerina@gmail.com>
2023-04-13 11:03:43 +0200

    Update Tree.scala

--- src/main/scala/Chat/Tree.scala
+++ src/main/scala/Chat/Tree.scala
@@ -9,7 +9,17 @@ sealed trait ExprTree
   * Declarations of the nodes' types.
   */
 object ExprTree:
-  // TODO - Part 2 Step 3
+  // TODO - Part 2 Step 3  
   // Example cases
+  case object Hello extends ExprTree
   case object Thirsty extends ExprTree
   case object Hungry extends ExprTree
+  case object GetBalance extends ExprTree
+  case class GetPrice(products: ExprTree) extends ExprTree
+  case class Order(products: ExprTree) extends ExprTree
+  case class Identify(pseudo: String) extends ExprTree
+  case class And(left: ExprTree, right: ExprTree) extends ExprTree
+  case class Or(left: ExprTree, right: ExprTree) extends ExprTree
+
+
+end ExprTree
