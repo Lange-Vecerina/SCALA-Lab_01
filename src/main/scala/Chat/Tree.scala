21cf1154211ce48850c0d4c41867fafadbbf5704
IvanR-Vecerina <IvanR.vecerina@gmail.com>
2023-04-17 17:40:38 +0200

    Update Tree.scala

--- src/main/scala/Chat/Tree.scala
+++ src/main/scala/Chat/Tree.scala
@@ -8,9 +8,7 @@ sealed trait ExprTree
 /**
   * Declarations of the nodes' types.
   */
-object ExprTree:
-  // TODO - Part 2 Step 3  
-  // Example cases
+object ExprTree: 
   case object Hello extends ExprTree
   case object Thirsty extends ExprTree
   case object Hungry extends ExprTree
